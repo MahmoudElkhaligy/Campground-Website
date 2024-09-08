85d7829aeeab7716cc9307d197e5beabf3578395
Mahmoud <melkhaligy77@gmail.com>
2024-09-08 16:20:59 +0300

    Initial commit

---
+++ middleware.js
@@ -0,0 +1,63 @@
+const Campground = require("./models/campground");
+const Review = require('./models/review.js');
+const ExpressError = require('./Utils/ExpressError');
+const { campgroundSchema, reviewSchema } = require('./scheams.js')
+
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'you must be signed in');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        req.flash("error", "Campground does not exist");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    if (!campground.author.equals(req.user._id)) {
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
